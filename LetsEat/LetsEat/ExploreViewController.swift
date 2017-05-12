6d5cde61cbccfb4064d3f59680f7b9cf7211b674
David Chin <dlcmhd@me.com>
2017-05-12 18:38:31 +0800

    collection view controller, set custom size for UICollectionViewCell

---
+++ LetsEat/LetsEat/ExploreViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by David Chin on 12/05/2017.
+//  Copyright © 2017 David Chin. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
